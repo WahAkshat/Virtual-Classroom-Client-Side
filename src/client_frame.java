76ca0a086b3ebcb6545c90b7bcefbda53c33429d
WahAkshat <jeet11chatterjee@gmail.com>
2020-10-25 22:32:13 +0530

    The chat feature after receiving file is working now

--- src/client_frame.java
+++ src/client_frame.java
@@ -8,7 +8,7 @@ import java.util.Random;
 
 public class client_frame extends javax.swing.JFrame
 {
-
+    //global variables
     String username, address = "localhost",type;
     ArrayList<String> users = new ArrayList();
     int port = 0007;
@@ -16,30 +16,28 @@ public class client_frame extends javax.swing.JFrame
     static Socket sock;
     BufferedReader reader;
     PrintWriter writer;
-    //--------------------------//
+    static String File_name;
+    static String Location;
+
     public void ListenThread()
     {
         Thread IncomingReader = new Thread(new IncomingReader());
         IncomingReader.start();
     }
-    //--------------------------//
     public void userAdd(String data)
     {
         users.add(data);
     }
-    //--------------------------//
     public void userRemove(String data)
     {
         ta_chat.append(data + " is now offline.\n");
     }
-    //--------------------------//
     public void writeUsers()
     {
         String[] tempList = new String[(users.size())];
         users.toArray(tempList);
         //users.append(token + "\n");
     }
-    //--------------------------//
     public void sendDisconnect()
     {
         String bye = (username + "- -Disconnect");
@@ -52,7 +50,6 @@ public class client_frame extends javax.swing.JFrame
             ta_chat.append("Could not send Disconnect message.\n");
         }
     }
-    //--------------------------//
     public void Disconnect()
     {
         try
@@ -66,101 +63,82 @@ public class client_frame extends javax.swing.JFrame
         tf_username.setEditable(true);
     }
 
-   static String File_name;
-   static String Location;
-
-    public static void sendFile() {
+    //function to send file to server
+    public static void sendFile()
+    {
         try {
-
             File myFile = new File(Location);
             byte[] mybytearray = new byte[(int) myFile.length()];
             if(!myFile.exists()) {
                 System.out.println("File does not exist..");
                 return;
             }
-
             FileInputStream fis = new FileInputStream(myFile);
             BufferedInputStream bis = new BufferedInputStream(fis);
-            //bis.read(mybytearray, 0, mybytearray.length);
-
             DataInputStream dis = new DataInputStream(bis);
             dis.readFully(mybytearray, 0, mybytearray.length);
-
-            System.out.println(Arrays.toString(mybytearray));
-
+            //System.out.println(Arrays.toString(mybytearray));
             OutputStream os = sock.getOutputStream();
 
-            //Sending file name and file size to the server
+            //sending file name and file size to server
             DataOutputStream dos = new DataOutputStream(os);
             dos.writeUTF(myFile.getName());
             dos.writeLong(mybytearray.length);
             dos.write(mybytearray, 0, mybytearray.length);
             dos.flush();
-            System.out.println("File "+Location+" sent to Server.");
-        } catch (Exception e) {
+            //System.out.println("File "+Location+" sent to Server.");
+        } catch (Exception e){
             System.err.println("Exceptionnnn: "+e);
         }
     }
 
-    public void receiveFile() {
+    //function to receive file from server
+    public void receiveFile()
+    {
         try {
-
             int bytesRead;
-
             InputStream in = sock.getInputStream();
-
             DataInputStream clientData = new DataInputStream(in);
 
-            System.out.println(File_name);
-
+            //receiving file name and file size from server
+            File_name = clientData.readUTF();
+            //System.out.println(File_name);
             OutputStream output = new FileOutputStream(File_name);
-
+            long size = clientData.readLong();
             byte[] buffer = new byte[1024];
-
-            int a = clientData.read(buffer);
-            System.out.println(Arrays.toString(buffer));
-            for(byte b:buffer){
-                if(b!=0)
-                    output.write(b);
+            while (size > 0 && (bytesRead = clientData.read(buffer, 0, (int) Math.min(buffer.length, size))) != -1) {
+                output.write(buffer, 0, bytesRead);
+                size -= bytesRead;
             }
-
             output.flush();
-
-            System.out.println("File "+File_name+" received from Server.");
-           } catch (IOException ex) {
+            output.close();
+            //System.out.println("File "+File_name+" received from Server.");
+        }catch(IOException ex)
+        {
             System.out.println("Exception: "+ex);
         }
-
-        ListenThread();
-
-
-
     }
+
     public client_frame()
     {
         initComponents();
     }
-    //--------------------------//
     public class IncomingReader implements Runnable
     {
         @Override
         public void run()
         {
             String[] data;
-            String stream, done = "Done", connect = "Connect", disconnect = "Disconnect", chat =
-                    "Chat",send="Send",receive="Receive";
-
+            String stream, done = "Done", connect = "Connect", disconnect = "Disconnect", chat = "Chat", send = "Send", receive = "Receive";
             try
             {
                 while ((stream = reader.readLine()) != null)
                 {
                     data = stream.split("-");
-
                     if (data[2].equals(chat))
                     {
-                        ta_chat.append(data[0] + "- " + data[1] + "\n");
+                        ta_chat.append(data[0] + "-" + data[1] + "\n");
                         ta_chat.setCaretPosition(ta_chat.getDocument().getLength());
-                        System.out.println(ta_chat.getText());
                     }
                     else if (data[2].equals(connect))
                     {
@@ -178,25 +156,19 @@ public class client_frame extends javax.swing.JFrame
                     }
 
                     else if (data[2].equals(send)) {
-                        System.out.println("Yes send till this line 181");
                         ta_chat.append(data[0] + "-" + data[1] + "-" + send +"\n");
                         ta_chat.setCaretPosition(ta_chat.getDocument().getLength());
-                        System.out.println(ta_chat.getText());
-
                     }
                     else if (data[2].equals(receive)) {
-                        System.out.println("Yes received at this line 188");
-                        System.out.println(data[1]);
                         ta_chat.append(data[0] + "-" + data[1] + "-" + receive +"\n");
                         ta_chat.setCaretPosition(ta_chat.getDocument().getLength());
-                        System.out.println(ta_chat.getText());
-                                            }
+                    }
                 }
-            }catch(Exception ex) { }
+            }catch(Exception ex){ }
         }
     }
 
-
+    //user interface design
     private void initComponents() {
         lb_address = new javax.swing.JLabel();
         lb_Name_url=new javax.swing.JLabel("FILE-DETAILS :");
@@ -217,13 +189,12 @@ public class client_frame extends javax.swing.JFrame
         setTitle("Chat - Client's frame");
         setName("client");
         setResizable(false);
-
         lb_address.setText("TYPE : ");
-//        tf_address.addActionListener(new java.awt.event.ActionListener() {
-//            public void actionPerformed(java.awt.event.ActionEvent evt) {
-//                tf_addressActionPerformed(evt);
-//            }
-//        });
+        /*tf_address.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                tf_addressActionPerformed(evt);
+            }
+        });*/
         lb_username.setText("NAME :");
         tf_username.getBorder();
         tf_username.addActionListener(new java.awt.event.ActionListener() {
@@ -231,7 +202,6 @@ public class client_frame extends javax.swing.JFrame
                 tf_usernameActionPerformed(evt);
             }
         });
-
         b_sendFile.setText("SEND FILE");
         b_sendFile.setBackground(Color.MAGENTA);
         b_sendFile.getBorder();
@@ -240,7 +210,6 @@ public class client_frame extends javax.swing.JFrame
                 b_sendFileActionPerformed(evt);
             }
         });
-
         b_receiveFile.setText("RECEIVE FILE");
         b_receiveFile.setBackground(Color.CYAN);
         b_receiveFile.getBorder();
@@ -253,8 +222,6 @@ public class client_frame extends javax.swing.JFrame
                 }
             }
         });
-
-
         b_connect.setText("CONNECT");
         b_connect.setBackground(Color.GREEN);
         b_connect.getBorder();
@@ -271,8 +238,6 @@ public class client_frame extends javax.swing.JFrame
                 b_disconnectActionPerformed(evt);
             }
         });
-
-
         ta_chat.setColumns(20);
         ta_chat.setRows(5);
         jScrollPane1.setViewportView(ta_chat);
@@ -284,9 +249,7 @@ public class client_frame extends javax.swing.JFrame
             }
         });
         lb_name.setText("OS PROJECT");
-        lb_name.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0,
-                0)));
-
+        lb_name.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -294,33 +257,25 @@ public class client_frame extends javax.swing.JFrame
                         .addGroup(layout.createSequentialGroup()
                                 .addContainerGap()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-
                                         .addGroup(layout.createSequentialGroup()
                                                 .addComponent(tf_chat, javax.swing.GroupLayout.PREFERRED_SIZE, 352,
                                                         javax.swing.GroupLayout.PREFERRED_SIZE)
                                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                                 .addComponent(b_send, javax.swing.GroupLayout.DEFAULT_SIZE, 111,
                                                         Short.MAX_VALUE))
-
                                         .addComponent(jScrollPane1)
-
                                         .addGroup(layout.createSequentialGroup()
-
 
                                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                                                         .addComponent(lb_username, javax.swing.GroupLayout.PREFERRED_SIZE,
                                                                 62, Short.MAX_VALUE)
                                                         .addComponent(lb_address, javax.swing.GroupLayout.DEFAULT_SIZE,
                                                                 javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-
                                                 .addGap(18, 18, 18)
-
                                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                                         .addComponent(tf_address, javax.swing.GroupLayout.DEFAULT_SIZE, 89,
                                                                 Short.MAX_VALUE)
-
                                                         .addComponent(tf_username))
-
                                                 .addGap(18, 18, 18)
 
                                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
@@ -331,26 +286,17 @@ public class client_frame extends javax.swing.JFrame
                                                         .addComponent(tf_Name_url, javax.swing.GroupLayout.PREFERRED_SIZE,
                                                                 62, Short.MAX_VALUE))
 
-
-
-
-
-
                                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false))
                                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false))
                                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-
                                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                                         .addGroup(layout.createSequentialGroup()
-
                                                                 .addComponent(b_connect)
                                                                 .addGap(50, 50, 50)
                                                                 .addComponent(b_disconnect)
                                                                 .addGap(50, 50, 50))
-
                                                 )
-
                                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                                         .addGroup(layout.createSequentialGroup()
 
@@ -358,11 +304,8 @@ public class client_frame extends javax.swing.JFrame
                                                                 .addGap(50, 50, 50)
                                                                 .addComponent(b_receiveFile)
                                                                 .addGap(50, 50, 50))
-
                                                 )
-
                                         )
-
                                 )
                                 .addContainerGap())
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING,
@@ -404,48 +347,40 @@ public class client_frame extends javax.swing.JFrame
                                 .addComponent(lb_name))
         );
         pack();
-    }// </editor-fold>
-    //    private void tf_addressActionPerformed(java.awt.event.ActionEvent evt) {
-//    }
+    }
+
+    //button to send file to server
     private void b_sendFileActionPerformed(java.awt.event.ActionEvent evt) {
         Location=tf_Name_url.getText();
-
-
         try {
             writer.println(type+" "+ username + "-" + Location + "-" + "Send");
-            writer.flush(); // flushes the buffer
+            writer.flush();
         }
         catch (Exception ex) {
             ta_chat.append("File was not sent. \n");
         }
-
         tf_Name_url.setText("");
         tf_Name_url.requestFocus();
 
         sendFile();
-
     }
 
-
+    //button to receive file from server
     private void b_receiveFileActionPerformed(java.awt.event.ActionEvent evt) throws FileNotFoundException {
         File_name=tf_Name_url.getText();
-
-
         try {
             writer.println(type+" "+ username + "-" + tf_Name_url.getText() + "-" + "Receive");
-            writer.flush(); // flushes the buffer
-            System.out.println("done till here");
+            writer.flush();
         }
         catch (Exception ex) {
             ta_chat.append("File was not received. \n");
         }
-
         tf_Name_url.setText("");
         tf_Name_url.requestFocus();
 
         receiveFile();
-
     }
+
     private void tf_usernameActionPerformed(java.awt.event.ActionEvent evt) {
     }
     private void b_connectActionPerformed(java.awt.event.ActionEvent evt) {
@@ -481,37 +416,6 @@ public class client_frame extends javax.swing.JFrame
         Disconnect();
     }
     private void b_anonymousActionPerformed(java.awt.event.ActionEvent evt) {
-        tf_username.setText("");
-        if (isConnected == false)
-        {
-            String anon="anon";
-            Random generator = new Random();
-            int i = generator.nextInt(999) + 1;
-            String is=String.valueOf(i);
-            anon=anon.concat(is);
-            username=anon;
-            tf_username.setText(anon);
-            tf_username.setEditable(false);
-            try
-            {
-                sock = new Socket(address, port);
-                InputStreamReader streamreader = new InputStreamReader(sock.getInputStream());
-                reader = new BufferedReader(streamreader);
-                writer = new PrintWriter(sock.getOutputStream());
-                writer.println(type+" "+ anon + "-has connected.-Connect");
-                writer.flush();
-                isConnected = true;
-            }
-            catch (Exception ex)
-            {
-                ta_chat.append("Cannot Connect! Try Again. \n");
-                tf_username.setEditable(true);
-            }
-            ListenThread();
-        } else if (isConnected == true)
-        {
-            ta_chat.append("You are already connected. \n");
-        }
     }
     private void b_sendActionPerformed(java.awt.event.ActionEvent evt) {
         String nothing = "";
@@ -522,7 +426,7 @@ public class client_frame extends javax.swing.JFrame
 
             try {
                 writer.println(type+" "+ username + "-" + tf_chat.getText() + "-" + "Chat");
-                writer.flush(); // flushes the buffer
+                writer.flush();
             } catch (Exception ex) {
                 ta_chat.append("Message was not sent. \n");
             }
@@ -543,15 +447,12 @@ public class client_frame extends javax.swing.JFrame
                 cl.setVisible(true);
                 cl.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 cl.setPreferredSize(new Dimension(550, 300));
-
-
                 cl.getContentPane().setBackground(Color.ORANGE);
             }
         });
     }
 
-
-    // Variables declaration - do not modify
+    // variables declaration
     private javax.swing.JButton b_sendFile;
     private javax.swing.JButton b_receiveFile;
     private javax.swing.JButton b_connect;
@@ -567,5 +468,4 @@ public class client_frame extends javax.swing.JFrame
     private javax.swing.JTextField tf_address;
     private javax.swing.JTextField tf_chat;
     private javax.swing.JTextField tf_username;
-// End of variables declaration
 }
